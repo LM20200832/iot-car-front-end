81c6c74ead3639bc13b8e45142c290acce0d7435
LM20200832 <morales.luis.3bm2017@gmail.com>
2024-05-28 12:31:14 -0600

    Update main.js

--- control/web/js/main.js
+++ control/web/js/main.js
@@ -13,7 +13,7 @@ function callApi(estatus) {
   // Hacer una petición para un usuario con ID especifico
   axios
     .get(
-      "https://3.128.180.251/iot-car-control/back-end/apis/setRegistro.php?valorEstatus=" +
+      "https://3.14.66.97/iot-car-control/back-end/apis/setRegistro.php?valorEstatus=" +
         estatus
     )
     .then(function (response) {
